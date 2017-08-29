0ccc400ec240df9c6eaa96f7a35f403a73d921b1
lmlewy <lewy24@interia.pl>
2017-08-29 21:28:01 +0200

    2

---
+++ r1/FtoC/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN 0
+#define MAX 100
+#define STEP 10
+
+int main()
+{
+
+
+    printf("Fahr na Celc\n");
+
+    float i = MIN;
+    float cels = 0.0;
+    while(i <= MAX)
+    {
+        cels = (5.0/9.0)*(i-32.0);
+        printf("%3.2f = %3.2f\n",i,cels);
+        i += STEP;
+    }
+
+
+
+
+    return 0;
+}
